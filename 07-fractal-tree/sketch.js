6d6a722293f0b83e0d8225e09a9f7aad7fa384a2
Alexander V. Groshev <null@sattellite.me>
2019-11-19 09:16:49 +0300

    Redraw fractal tree on click

--- 07-fractal-tree/sketch.js
+++ 07-fractal-tree/sketch.js
@@ -1,15 +1,15 @@
 // From here https://youtu.be/0jjeOYMjmDU
-const angle = 3.14159265358979323846 / 3.5;
-const step = 0.666666667;
+let angle = 3.14159265358979323846 / 3.5;
+let step = 0.666666667;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   stroke(255);
+  push();
   translate(windowHeight / 2, windowHeight);
   branch(windowHeight / 4);
+  pop();
 }
-
-function draw() {}
 
 function branch(len) {
   line(0, 0, 0, -len);
@@ -24,4 +24,17 @@ function branch(len) {
     branch(len * step);
     pop();
   }
+}
+
+function mouseClicked() {
+  angle = random(3.14159265358979323846 / 3, 3.14159265358979323846 / 4);
+  step = random(0.5, 0.75);
+  console.log({ angle, step });
+  push();
+  clear();
+  background(0);
+  stroke(255);
+  translate(windowHeight / 2, windowHeight);
+  branch(windowHeight / 4);
+  pop();
 }
