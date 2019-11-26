21df19a9ccd565af4ce0326672175828d5dac5cf
Alexander V. Groshev <sattellite@insystem.su>
2019-11-26 14:32:45 +0300

    Fix position of fractal tree

--- 07-fractal-tree/sketch.js
+++ 07-fractal-tree/sketch.js
@@ -1,17 +1,24 @@
 // From here https://youtu.be/0jjeOYMjmDU
-let angle = 3.14159265358979323846 / 3.5;
-let step = 0.666666667;
+let angle, step;
 function setup() {
+  angle = random(3.14159265358979323846 / 3, 3.14159265358979323846 / 4);
+  step = random(0.5, 0.75);
+  console.log({ angle, step });
   createCanvas(windowWidth, windowHeight);
   background(0);
   stroke(255);
   push();
-  translate(windowHeight / 2, windowHeight);
+  translate(windowWidth / 2, windowHeight);
   branch(windowHeight / 4);
   pop();
 }
 
 function branch(len) {
+  let weight = len / 10;
+  if (weight < 1) {
+    weight = 1;
+  }
+  strokeWeight(weight);
   line(0, 0, 0, -len);
   translate(0, -len);
   if (len > 2) {
@@ -27,14 +34,5 @@ function branch(len) {
 }
 
 function mouseClicked() {
-  angle = random(3.14159265358979323846 / 3, 3.14159265358979323846 / 4);
-  step = random(0.5, 0.75);
-  console.log({ angle, step });
-  push();
-  clear();
-  background(0);
-  stroke(255);
-  translate(windowHeight / 2, windowHeight);
-  branch(windowHeight / 4);
-  pop();
+  setup();
 }
